b1aba066bf3190365db843f60694617bd3af604e
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-11-19 18:36:09 +0000

    added account icon for when user is not signed in

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -40,14 +40,14 @@ export default function NavLinks({session}: {session: any}) {
             href={link.href}
             className="navlink"
           >            
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden md:block menu-dropdown-toggle">{link.name}</p>
           </Link>
         );
       })}
       <Link
         href={'/account'}
       >
-        <div className="account-icon">
+        <div className="account-icon flex justify-center">
           {session ? 
           <Image
             src={session.user.image}
@@ -57,7 +57,9 @@ export default function NavLinks({session}: {session: any}) {
             style={{border: 'black solid 1px', borderRadius: '50%'}}
           ></Image>
           :
-          null        
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="50" height="50">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>     
         }
 
         </div>
@@ -67,6 +69,8 @@ export default function NavLinks({session}: {session: any}) {
 }
 
 /*
+//this was from the svg for the account icon
+className="size-6"
 
 This was a link prop:
 
