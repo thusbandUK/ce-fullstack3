3594a06190620d4f362d8fbea26429ba5386c11e
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-11-25 09:14:52 +0000

    added Modal component to render modal on click of active MenuItems

---
+++ app/ui/dashboard/modal.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+export default function Modal({identifier, receiveEmail}: {identifier: number, receiveEmail: boolean | null}){
+
+    return (
+        <>
+        {/* Put this part before </body> tag */}
+
+          <input type="checkbox" id={`my_modal_${identifier}`} className="modal-toggle" />
+          <div className="modal" role="dialog">
+            <div className="modal-box">
+              <h3 className="text-lg font-bold">Hello!</h3>
+              <p className="py-4">I am evil Homer!</p>
+              <Link
+                href="/login?location=/flashcards"
+              >
+                login
+              </Link>
+              <Link
+                href="/flashcards"
+              >
+                Go back
+              </Link>
+            </div>
+            <label className="modal-backdrop" htmlFor={`my_modal_${identifier}`}>Close</label>
+          </div>
+        </>
+    )
+}
