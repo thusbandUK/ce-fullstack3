8306ee6217449993a517cb6b97e8ca4f03762738
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-11-26 08:26:57 +0000

    updated Modal component to render black text even when called from menuItem with greyed out text

--- app/ui/dashboard/modal.tsx
+++ app/ui/dashboard/modal.tsx
@@ -7,7 +7,7 @@ export default function Modal({identifier}: {identifier: number}){
         {/* Put this part before </body> tag */}
 
           <input type="checkbox" id={`my_modal_${identifier}`} className="modal-toggle" />
-          <div className="modal" role="dialog">
+          <div className="modal text-black" role="dialog">
             <div className="modal-box">
               <h3 className="text-lg font-bold">Coming soon!</h3>
               <p className="py-4">Sorry, flashcards for this examboard are not available yet, but don&#39;t worry, your interest has been logged.
