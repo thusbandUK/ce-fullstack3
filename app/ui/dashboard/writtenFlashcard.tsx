c565fd5cb9a66ff65cf402a17ae8afe6fdc2d210
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-12-12 15:05:12 +0000

    styled written flashcard individual response checklist

--- app/ui/dashboard/writtenFlashcard.tsx
+++ app/ui/dashboard/writtenFlashcard.tsx
@@ -68,19 +68,14 @@ const WrittenFlashcard = (
       //crosses or uncrosses the checklist point in the correct location in the ResponseAssessmentContext
       return responseAssessment[index].checkedPoints[event.target.value as keyof assessedResponse["checkedPoints"]] = checked;      
     }
-
-    //flex-col justify-between 
-    /*
-    <div className="grid grid-cols-6 w-100 mx-auto mt-16">
-        <div className="col-start-1 rounded-lg col-span-6 flex" style={{border: 'black solid 1px'}}>
-    */
+    
     return (
         <div className="w-full md:w-4/5 flex flex-col px-2 pb-4 mx-auto grid grid-cols-6">
           <div className="border-2 w-full flex flex-col border-black rounded-lg px-5 py-1 m-auto col-start-1 col-span-6">
             <div className="spacer"></div>
             <h1
               dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(question)}}
-              className="text-center"
+              className="md:text-center"
             ></h1>
             <div className="spacer"></div>
           </div>
@@ -95,7 +90,7 @@ const WrittenFlashcard = (
                       onChange={handleResponseChange}
                       name="response"
                       rows={5} cols={33}
-                      className="border-2 border-black rounded-lg p-5 w-full h-full"
+                      className="border-2 border-black rounded-lg p-5 md:p-5 w-full h-full"
                       placeholder="Write your answer here..."
                     >
                 
@@ -103,7 +98,7 @@ const WrittenFlashcard = (
                     </div>
                     <div className="col-start-1 md:col-start-5 col-span-6 md:col-span-2 border-2 border-black rounded-lg flex flex-col justify-end">
                       <button  onClick={submitResponse}>
-                    <label htmlFor="response" >
+                    <label htmlFor="response" className="cursor-pointer">
                       <div className="m-5">
                       <ArrowCommand 
                         borderGray={false}
@@ -119,36 +114,60 @@ const WrittenFlashcard = (
                 
             : null}
             { (writtenStage === "feedback") ?
-              <div>
+            <>
+              
             
-                <legend>Check the points you got right</legend>
-                <h2>Model answer:</h2>
+              <div className="border-2 border-black rounded-lg col-start-1 col-span-6 w-full p-5 md:p-10">  
+                <h2 className="text-xl md:text-4xl">Model answer:</h2>
                 <p
                   dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(definition)}}
+                  className="pt-5"
                 ></p>
-                <h2>Your answer:</h2>
-                <p>{responseAssessment ? responseAssessment[index].response : null}</p>
+              </div>
+              <div className="border-2 border-black rounded-lg col-start-1 col-span-6 md:col-span-3 w-full p-5 md:p-10">
+                <h2 className="text-xl md:text-4xl">Your answer:</h2>
+                <p className="pt-5">{responseAssessment ? responseAssessment[index].response : null}</p>
+              </div>
+              <div className="border-2 border-black rounded-lg col-start-1 md:col-start-4 col-span-6 md:col-span-3 w-full p-5 md:p-10">
               
                 {noNullItemsChecklist.map((x: string) => {
                   const arrayIndex = checklist.findIndex((item: string) => item === x);                  
                   return (
-                    <div key={arrayIndex}>
-                      <input                        
-                        onChange={handleCheckboxChange}
-                        type="checkbox" 
-                        id={`checkbox-${arrayIndex}`}                        
-                        value={numericalIndexToLetter(arrayIndex)} 
-                        name={`checkbox-${arrayIndex}`} />
+                    <div key={arrayIndex} className="flex">
+                      
                       <label 
                         htmlFor={`checkbox-${arrayIndex}`}
-                        dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(x)}}
-                      ></label>
+                        className="label items-start"
+                      >
+                        <input                        
+                          onChange={handleCheckboxChange}
+                          type="checkbox" 
+                          id={`checkbox-${arrayIndex}`}                        
+                          value={numericalIndexToLetter(arrayIndex)} 
+                          name={`checkbox-${arrayIndex}`}
+                          className="mt-1 me-1 bg-elephant-violet"
+                        />
+                        <p
+                          dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(x)}}
+                          className="label-text"
+                        ></p>
+                      </label>
                     </div>      
                 )})}              
               
-                <button onClick={submitChecklist}>Submit</button>
-              </div>                           
-              
+              <button  onClick={submitChecklist}>
+                    <label htmlFor="response" className="cursor-pointer">
+                      <div className="mt-5">
+                      <ArrowCommand 
+                        borderGray={false}
+                        command="SUBMIT"
+                      />   
+                      </div>                   
+                    </label>
+                    </button>
+                
+              </div>
+              </>
             : null
             }
         </div>
@@ -156,5 +175,11 @@ const WrittenFlashcard = (
 }
 
 export default WrittenFlashcard;
+
+/*
+<legend>Check the points you got right</legend>
+
+<button onClick={submitChecklist}>Submit</button>
+*/
 
 //<button onClick={submitResponse}>Submit</button>
