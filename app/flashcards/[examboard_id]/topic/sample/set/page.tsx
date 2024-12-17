0fb649b66254e2520f93ca2eeef1053ba9d28b0f
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-12-17 10:20:21 +0000

    removed container styling in complementaryTopic page.tsx

--- app/flashcards/[examboard_id]/topic/sample/set/page.tsx
+++ app/flashcards/[examboard_id]/topic/sample/set/page.tsx
@@ -4,9 +4,9 @@ import FlashcardPresentation from "@/app/ui/dashboard/flashcards";
 export default async function Page({ params }: { params: { examboard_id: string } }) {    
 
     const allFlashcardsData = await fetchComplementaryTopic(params.examboard_id) 
-
+//w-11/12 md:w-4/5 mx-auto mt-10
 return (
-    <div className="w-11/12 md:w-4/5 mx-auto mt-10">
+    <div className="">
         {allFlashcardsData.length === 0 ?
         null:
         <FlashcardPresentation
