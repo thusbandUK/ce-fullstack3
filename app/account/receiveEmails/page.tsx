0797dfe8c6637dd9afaabfe6bae0904cbef794d6
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-09-30 14:45:21 +0100

    new page to modify newsletter status

---
+++ app/account/receiveEmails/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import UpdateUsername from "@/app/ui/updateUsername";
+import SignUpNewsletter from '@/app/ui/signUpNewsletter';
+
+export default async function ReceiveEmail({ searchParams }: { searchParams: { location: string } }){
+
+    const session: any = await auth();
+
+    if (!session){
+      redirect(`/login`);
+    }
+
+    return (
+        <div>
+          <h1>Sign up for newsletters</h1>
+          <SignUpNewsletter
+            email={session.user.email}
+            username={session.user.name}
+            location={searchParams.location ? searchParams.location : ''}
+            receivingNewsletter={session.user.receive_email}
+          ></SignUpNewsletter>
+        </div>
+    )
+}
