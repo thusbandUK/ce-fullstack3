f1730a56de82cad4987da2cfb6f89b5f399cec23
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-09-30 11:06:22 +0100

    debug removed import of pool from deleted databaseConfig file

--- authConfig.ts
+++ authConfig.ts
@@ -1,8 +1,8 @@
 //this wasn't part of the previous set up
 
 import {NextAuthConfig} from "next-auth";
-import PostgresAdapter from "@auth/pg-adapter"
-import pool from "./databaseConfig";
+//import PostgresAdapter from "@auth/pg-adapter"
+//import pool from "./databaseConfig";
 /*import { Pool } from "@neondatabase/serverless"*/
 
 
