d19eb99db90390418f0bd94f18ee116b3065e441
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-02 06:46:25 +0100

    updated first login logic so that account page automatically redirects to welcome page if session object has no username

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,34 +1,46 @@
-import SignOut from "../ui/dashboard/signOut"
+import SignOut from "../ui/dashboard/signOut";
 import { SignIn } from '../ui/login-form';
 import { auth } from '@/auth';
 import Link from "next/link";
 import { redirect } from 'next/navigation';
 
-export default async function Account(){
+export default async function Account({ searchParams }: { searchParams: { location: string } }){
 
     const session: any = await auth();
 
-    if (!session){      
+    if (!session){
       redirect(`/account/login`);
+    }
+
+    if (!session.user.name){
+      if (!searchParams.location){
+        redirect('/account/welcome/signup');
+      } else {
+        redirect(`/account/welcome/signup?location=${searchParams.location}`);
+      }
+    }
+
+    if (session){
+      if (searchParams.location){
+        redirect(searchParams.location)
+      }
     }
 
     return (
         <div>
-            { session ? `Hello, ${session.user.name}` : null}
-            { session ?            
-              <SignOut />              
-            : 
-            <SignIn 
+            { session ? `Welcome back, ${session.user.name}!` : null}
+            { session ?
+              <SignOut />
+            :
+            <SignIn
               location="/account"
             />}
             <Link
               href="/account/username"
             >Change username</Link>
             <Link
-              href="/account/delete"            
-            >Delete account</Link>
-            
-            
+              href="/account/delete"
+            >Delete account</Link>            
         </div>
     )
 }
