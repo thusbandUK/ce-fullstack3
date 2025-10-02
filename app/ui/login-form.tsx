d15436c7a54922b306b505f76bffc369ade2abad
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-02 07:21:15 +0100

    implemented locationParser function to optimise login callbackUrl logic

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,6 @@
 import { Button } from './button';
 import { signIn } from "@/auth";
+import { locationParser } from '../lib/functions';
 
 export function SignIn({
   location,
@@ -8,7 +9,7 @@ export function SignIn({
 }: { location: string | null, provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   
   /*This function effectively renders a button (on the login page), which directs the user to the 
-  signin page (?)
+  signin page
 
   function here:
   await signIn(provider, { redirectTo: whereToRedirect});
@@ -18,11 +19,24 @@ export function SignIn({
                 redirectTo: props.searchParams?.callbackUrl ?? "",
               })  
   
-  The fucntion is passed the location via imperfect nextauth code with the following drawback:
-  if there is no location, ie: user clicks the account to sign in rather than being redirected from,
-  eg: the flashcards menu, an empty string will be passed. The code immediately below passes the
-  relative link /welcome if the location string is empty, but if an actual location has been passed,
-  it passes that on to the redirectTo option in the signin function below
+  The function is passed the location via imperfect nextauth code with the following drawbacks:
+
+  1) the code has a bug so that there's this bizarre infinite regression link generate with a callbackUrl
+  within a callbackUrl. This is addressed by the function cleanUpUrl in authFunctions.ts
+
+  2) if there is no location, ie: user clicks the account to sign in rather than being redirected from,
+  eg: the flashcards menu, the param value is stored as undefined. Meanwhile, TypeScript likes to think
+  of it potentially as being null. The imported function locationParser returns an empty string for any
+  null or undefined values, so that .length === 0 logic can be applied to omit location params from
+  the callbackUrl
+
+  The callbackUrl nextauth function will direct users who have just logged in to the /account root page.
+  If no location has been specified, the param is not included in the callbackUrl. If it is specified
+  it is included again as a location param for the user's landing on the account root page.
+
+  Note that if it is the user's first sign in, they are redirected to input their user name and confirm
+  their newsletter subscription settings before being redirected either back to the account root page
+  or to their pre-login location 
 
   Notice as well: there is a redirectTo option here and then another one in the signin page, this
   redirect (identified from params in parent login component as "location") then becomes the 
@@ -30,15 +44,8 @@ export function SignIn({
 
   */  
 
-  let whereToRedirect = "";
-  if (location !== null){
-    if (location.length === 0){
-      whereToRedirect = '/welcome'
-    } else {
-      whereToRedirect = location;
-    }
-
-  }
+  const parsedLocation = locationParser(location);
+  const whereToRedirect = parsedLocation.length === 0 ? '/account' : '/account?location=' + parsedLocation;
 
   return (
     <form
