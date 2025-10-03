305e069ebc9b39c250c4edb979c226edddcf2124
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-03 11:53:48 +0100

    removed ref to WrittenSummaryMock in overview/flashcards/mock to debug deploy

--- app/(overview)/flashcards/mock/page.tsx
+++ app/(overview)/flashcards/mock/page.tsx
@@ -7,7 +7,9 @@ import { incrementExamboard } from '@/app/lib/actions';
 import { Suspense } from "react";
 import { CardSkeleton, ExamSkeleton } from "@/app/ui/dashboard/skeletons";
 import DashboardSkeleton from '@/app/ui/dashboard/skeletons';
-import WrittenSummaryMock from '@/app/ui/dashboard/writtenSummaryMock';
+//not sure why it wasn't a problem before but the below refs an untracked file (in gitignore)
+//hence crashed deployment w error
+//import WrittenSummaryMock from '@/app/ui/dashboard/writtenSummaryMock';
 
 export default async function Page() {
 
