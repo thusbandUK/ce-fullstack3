005c6462cd39b7d564ca2337e06e2159353e67fb
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-03 11:43:51 +0100

    updated styling removed padding x-axis value as now applied in overview layout

--- app/ui/dashboard/writtenFlashcard.tsx
+++ app/ui/dashboard/writtenFlashcard.tsx
@@ -68,9 +68,9 @@ const WrittenFlashcard = (
       //crosses or uncrosses the checklist point in the correct location in the ResponseAssessmentContext
       return responseAssessment[index].checkedPoints[event.target.value as keyof assessedResponse["checkedPoints"]] = checked;      
     }
-    
+    //px-2 
     return (
-        <div className="w-full md:w-4/5 flex flex-col px-2 pb-4 mx-auto grid grid-cols-6">
+        <div className="w-full md:w-4/5 flex flex-col pb-4 mx-auto grid grid-cols-6">
           <div className="border-2 w-full flex flex-col border-black rounded-lg px-5 py-1 m-auto col-start-1 col-span-6">
             <div className="spacer"></div>
             <h1
