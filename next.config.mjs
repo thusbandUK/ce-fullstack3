992d9b857f3acbb624721aac044da7fc3761b7f1
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-07 11:41:49 +0100

    next config file contains a commented out webpack ref which could potentially help with debugging error

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,26 @@ const nextConfig = {
           },
         ],
       },
+      /*webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+      ) => {
+        //Below is from: https://www.reddit.com/r/nextjs/comments/1hu7j67/people_who_are_using_nextjs_with_css_modules_and/
+        //doesn't seem to help
+        /*config.plugins = [
+          ...(config.plugins ?? []),
+          new webpack.BannerPlugin({
+            banner: '@layer reset, pallete, layout, components, overrides;',
+            test: /\.s?css$/,
+            raw: true,
+            entryOnly: false,
+          }),
+        ];*//*
+        // Important: return the modified config
+        //console.log('does the webpack part of config fire during error?')
+        //console.log(JSON.stringify(defaultLoaders, null, 2));
+        return config
+      },*/
 };
 
 export default nextConfig;
