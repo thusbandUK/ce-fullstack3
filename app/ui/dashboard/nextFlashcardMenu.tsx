ff412156ab5c786dfeeee7babc375ad5496613e1
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-14 08:33:06 +0100

    nextFlashcardMenu component created, not yet functional

---
+++ app/ui/dashboard/nextFlashcardMenu.tsx
@@ -0,0 +1,11 @@
+
+
+export default function NextFlashcardMenu(){
+
+    return (
+        <div id="next-flashcard-menu" className="mt-3">
+            <h2>Try more flashcards</h2>
+            <p>This is where next flashcards menu will render</p>
+        </div>
+    )
+}
