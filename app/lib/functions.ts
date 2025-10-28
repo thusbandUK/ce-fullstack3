8869793577f3bc426a0c507afb9cb140e5decf6d
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-28 16:49:20 +0000

    styled renewDeleteRequest server comp and rendered client form, removed function from server comp into functions.ts

--- app/lib/functions.ts
+++ app/lib/functions.ts
@@ -28,6 +28,35 @@ export const locationParser = (location: string | null | undefined) => {
     }
 }
 
+export type paramsObject = {
+    again: string;
+}
+export const renewDeleteMessage = (params: paramsObject) => {
+    
+    let message: string = "";
+    const filler: string = "Press the button to send a new one";
+    if (params.again){
+        switch (params.again){
+          case "none":
+          message = `Sorry you have not received any message. ${filler}.`
+          break;
+          case "expired":
+          message = `Looks like your link expired. ${filler} and make sure to click the link within one hour.`
+          break;
+          case "corrupted":
+          message = `Looks like there's something wrong with your link. Curious! ${filler}.`
+          break;
+          case "already":
+          message = `We already have a record of you requesting an email to delete your account. ${filler}.`
+          break;
+          case "wrong":
+          message = `The credentials you have supplied do not match those we have stored. Curious! ${filler}.`
+        }
+      } else {
+        message = `Not sure what has happened but press the button if you need a new link to delete your account.`
+      }
+      return message
+}
 //takes an array of objects and returns an array of numbers in string format
 /*
 export type QuestionsData = {
