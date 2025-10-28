4ba6584e2ed3f5eaa04f18b75ee54b6a66214d16
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-28 11:15:14 +0000

    updated to receive disabled prop, which disables hover styling when true

--- app/ui/dashboard/arrowCommand.tsx
+++ app/ui/dashboard/arrowCommand.tsx
@@ -1,20 +1,42 @@
 import clsx from "clsx"
 
-export default function ArrowCommand({command, borderGray}: {command: string | null, borderGray: boolean}){
-
-  //console.log(`borderGray is ${borderGray}`);
+export default function ArrowCommand(
+  {
+    command,
+    borderGray,
+    disabled
+  }: {
+    command: string | null,
+    borderGray: boolean,
+    disabled: boolean
+  },
+  ){
 
     return (
         <>
           <div className="flex">
-            <div className={clsx("border-2 rounded-md w-fit my-auto p-2 hover:bg-black hover:text-white",
+            <div className={clsx("border-2 rounded-md w-fit my-auto p-2",
             {
               'border-black': borderGray === false,
               'border-slate-400': borderGray === true
+            },
+            {
+              'hover:bg-black hover:text-white': !disabled
             }
 
             )}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 hover:white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={clsx("size-8",
+                  {
+                    'hover:white': !disabled
+                  }
+                )}
+              >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
               </svg>
             </div>
