0230751a0d88c0ca77860a75531a410b50d16ecf
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-10-28 09:37:47 +0000

    commented out defunct functions

--- app/lib/functions.ts
+++ app/lib/functions.ts
@@ -1,5 +1,7 @@
 import { FlashcardData, QuestionsData } from "./definitions";
 
+//takes an array of strings and returns an array containing the same values but in a different sequence
+
 export const shuffle = (array: string[]) => { 
     for (let i = array.length - 1; i > 0; i--) { 
       const j = Math.floor(Math.random() * (i + 1)); 
@@ -8,26 +10,42 @@ export const shuffle = (array: string[]) => {
     return array; 
   };
 
-//returns string, receives an array of numbers in string format, returns a string of sql.query parameters
-//eg input ["5","8","9"] returns '$1, $2, $3'
-export const queryMaker = (listOfIds: string[]) => {
-    const outputArray: string[] = [];
-    for (let x = 0; x < (listOfIds.length - 1); x++){
-        outputArray.push(`$${(x + 1).toString()}, `)
+  /*
+Since there might not be a location in any given url, the param often comes back as undefined,
+but TypeScript expects it often also to be null, so this effectively stringifies location, returning
+an empty string "" if the param is passed as either null or undefined. This then enables .length === 0
+logic to be used effectively to ignore the location param when no location has been specified
+*/
+
+export const locationParser = (location: string | null | undefined) => {
+    if ((location !== undefined) && (location !== null) && (typeof location !== 'string')){
+        return "";
     }
-    //last value pushed to array omits ', '
-    outputArray.push(`$${listOfIds.length}`);
-    return outputArray.join("");
+    if ((location === undefined) || (location === null)){
+        return "";
+    } else {
+        return location;
+    }
 }
 
 //takes an array of objects and returns an array of numbers in string format
+/*
+export type QuestionsData = {
+  id: number;
+  topics_id: number;
+  question: number;
+
+}
+
 export const questionSetSimplifiedArray = (questionSetArrayOfObjects: QuestionsData[]) => {
     const questionSetArray = questionSetArrayOfObjects.map((x: QuestionsData) => {
         return x.question.toString();
     })
     return questionSetArray;
 }
+*/
 
+/*
 //takes an array of numbers in string format and selects 15 at random
 export const randomSelectionOfFifteen = (inputArray: string[]) => {
     //edge case - if there are fewer than 15 id values passed to the function, the below if clause
@@ -51,18 +69,19 @@ export const randomSelectionOfFifteen = (inputArray: string[]) => {
     }
     return outputArray;
 }
-
-/*
-Since there might not be a location in any given url, the param often comes back as undefined,
-but TypeScript expects it often also to be null, so this effectively stringifies location, returning
-an empty string "" if the param is passed as either null or undefined. This then enables .length === 0
-logic to be used effectively to ignore the location param when no location has been specified
 */
 
-export const locationParser = (location: string | null | undefined) => {
-    if ((location === undefined) || (location === null)){
-        return "";
-    } else {
-        return location;
+
+/*
+//returns string, receives an array of numbers in string format, returns a string of sql.query parameters
+//eg input ["5","8","9"] returns '$1, $2, $3'
+export const queryMaker = (listOfIds: string[]) => {
+    const outputArray: string[] = [];
+    for (let x = 0; x < (listOfIds.length - 1); x++){
+        outputArray.push(`$${(x + 1).toString()}, `)
     }
+    //last value pushed to array omits ', '
+    outputArray.push(`$${listOfIds.length}`);
+    return outputArray.join("");
 }
+*/
