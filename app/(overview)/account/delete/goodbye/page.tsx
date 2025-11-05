59c3effb74258669d0030f9f6de07648c052bd99
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-11-05 08:51:30 +0000

    styled

--- app/(overview)/account/delete/goodbye/page.tsx
+++ app/(overview)/account/delete/goodbye/page.tsx
@@ -1,9 +1,23 @@
+import HeaderDivs from "../../../../ui/dashboard/header";
+import LeftHandColumn from "../../../../ui/dashboard/leftHandColumn";
+import BottomRow from "../../../../ui/dashboard/bottomRow";
+import RightHandColumn from "../../../../ui/dashboard/rightHandColumn";
+
 const Goodbye = async () => {
-              
+
     return (
         <>
-          <h1>Goodbye!</h1>
-          <p>Sorry you are leaving. Hope you have found Chemistry Elephant useful. You know where we are if you need us again.</p>
+          <HeaderDivs h1Content="Goodbye!"></HeaderDivs>
+          <BottomRow>
+            <LeftHandColumn>
+              <p>Sorry you are leaving. Hope you have found Chemistry Elephant useful. You know where we are if you need us again.</p>
+            </LeftHandColumn>
+            <RightHandColumn>
+              <div className="m-5">
+                <p>INSERT ELEPHANT</p>
+              </div>
+            </RightHandColumn>
+          </BottomRow>
         </>
     )
 }
