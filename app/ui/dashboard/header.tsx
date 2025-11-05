aa79d43a4e3e8a72d6af0d315e628d0e5e855424
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-11-05 08:27:59 +0000

    created elements to streamline styling

---
+++ app/ui/dashboard/header.tsx
@@ -0,0 +1,11 @@
+export default function HeaderDivs({h1Content}: {h1Content: string}){
+    return (
+        <div className="w-full flex flex-col mx-auto grid grid-cols-6">
+          <div className="border w-full flex flex-col border-black rounded-lg px-5 py-1 m-auto col-start-1 col-span-6">
+            <div className="spacer"></div>
+            <h1>{h1Content}</h1>
+            <div className="spacer"></div>
+          </div>
+        </div>
+    )
+}
