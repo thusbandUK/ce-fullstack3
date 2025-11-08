7b75b79d97bd3501323099cffa9c9f393e38fea8
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-11-08 10:44:41 +0000

    custom sign in email

--- authCustomEmail.ts
+++ authCustomEmail.ts
@@ -36,6 +36,89 @@ export function customMailHtml(params: { encodedUpdatedCompleteUrl: string, host
   }
 
   return `
+  <main style="width: 90%;">
+    <table style="margin: auto; width: 100%;">
+        <tr>
+          <td style="border: solid black 1px; border-radius: 1.75rem;">
+            <h1 style="font-size: 1.75rem; padding: 1.75rem; text-align: center;">Chemistry Elephant</h1>
+          </td>
+        </tr>
+        
+      </table>
+      <table style="margin: auto; width: 100%;">
+        <tr>
+            
+            <td  style="width: 100%; border: 1px solid black; padding: 1rem; border-radius: 1.25rem; vertical-align: middle; text-align: center;">
+              <h2>Delete account</h2>
+              
+            </td>
+            <!--<td class="second-row-cell second-row-cell-3"></td>
+            
+            <td style="width: 30%; border: 1px solid black; border-radius: 1.75rem; position: relative;">
+                <div style="margin: auto; position: absolute; width: 80%;  left: 50%; top: 50%; transform: translate(-50%, -50%);">
+            <p>centred?</p>  
+              <div style="width: inherit;">
+                    <h2>Delete account</h2>
+                    <h1 style="font-size: 1.75rem; padding: 1.75rem; text-align: center;">Chemistry Elephant</h1>
+                </div>
+            -->
+        </tr>
+      </table>
+      <table style="margin: auto; width: 100%;">
+        <tr>
+          <td style="border: solid black 1px; border-radius: 1.25rem;">
+            <p style="padding: 1.75rem; text-align: center;">You have been sent this email because you opted to delete your account with Chemistry Elephant. To complete the process, click confirm below.</p>
+            
+          </td>
+        </tr>
+        
+      </table>
+      <table>
+        <tr>
+          <td style="width: 50%; border: 1px solid black; border-radius: 1.25rem; vertical-align: middle; text-align: center;">
+            <div style="width: 80%; margin: 0% auto; padding-top: 5%;">
+                <img src="http://${host}/pink-elephant-transparent-background.png" style="width: 100%; height: auto;">
+
+            </div>
+          </td>
+          <td style="width: 50%; border: 1px solid black; border-radius: 1.25rem; padding: 5%; vertical-align: bottom;">
+            <a
+              href="${encodedUpdatedCompleteUrl}"
+              style="cursor: pointer; text-decoration: none;"
+              target="_blank"
+            >
+            <table>
+              <tr>
+                <td style="width: 30%;">
+
+                  <div style="border: 1px solid black; border-radius: 1.25rem; padding: 15%; padding-top: 25%; ">
+                    <img src="http://${host}/right-arrow.png" style="width: 100%; height: auto;">
+                    </div>
+                </td>
+                <td style="vertical-align: middle; padding-left: 7.5%; color: black;">
+                  CONFIRM
+                </td>
+              </tr>
+            </table>
+          </a>
+
+          </td>
+        </tr>
+      </table>
+      
+  </main>
+  `
+  
+ 
+}
+
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+export function customEmailText({ encodedUpdatedCompleteUrl, host }: { encodedUpdatedCompleteUrl: string, host: string }) {
+  return `Sign in to ${host}\n${encodedUpdatedCompleteUrl}\n\n`
+}
+
+/*
+ `
 <body style="background: ${color.background};">
   <table width="100%" border="0" cellspacing="20" cellpadding="0"
     style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
@@ -66,9 +149,5 @@ export function customMailHtml(params: { encodedUpdatedCompleteUrl: string, host
   </table>
 </body>
 `
-}
 
-/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
-export function customEmailText({ encodedUpdatedCompleteUrl, host }: { encodedUpdatedCompleteUrl: string, host: string }) {
-  return `Sign in to ${host}\n${encodedUpdatedCompleteUrl}\n\n`
-}
+*/
