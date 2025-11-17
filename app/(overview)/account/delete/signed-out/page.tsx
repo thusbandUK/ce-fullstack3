6553e34e02b06c25feb1139226465c1e106300e8
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-11-17 17:02:23 +0000

    debug initiate signIn issue in signed-out/page

--- app/(overview)/account/delete/signed-out/page.tsx
+++ app/(overview)/account/delete/signed-out/page.tsx
@@ -25,6 +25,7 @@ export default async function Page(){
               <div className="m-5">
                 <InitiateSignIn
                   location={'account/delete/sent'}
+                  error={false}
                 />
               </div>
             </RightHandColumn>
