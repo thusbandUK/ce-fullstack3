0c62c331bbf825bc5c198c7e5d2705e555aebc3a
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2025-11-17 15:29:27 +0000

    full height added to left-hand column component

--- app/ui/dashboard/leftHandColumn.tsx
+++ app/ui/dashboard/leftHandColumn.tsx
@@ -1,7 +1,7 @@
 export default function LeftHandColumn({children}: {children: React.ReactNode}){
 
     return (
-      <div className="col-start-1 col-span-6 md:col-span-4 w-full flex flex-col border border-black rounded-lg p-5">
+      <div className="col-start-1 col-span-6 md:col-span-4 h-full w-full flex flex-col border border-black rounded-lg p-5">
         {children}
       </div>
     )
