5978d736b06381aab6e001011f91fc65271b8dab
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-01-15 16:31:29 +0000

    updated base url to env var in auth-client for google auth

--- auth-client.ts
+++ auth-client.ts
@@ -4,7 +4,8 @@ import { auth } from "./auth";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    //baseURL: "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [inferAdditionalFields<typeof auth>(
         {
             user: {
