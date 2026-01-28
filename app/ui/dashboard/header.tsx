54df2d8119dfc1cabe72725a292c9455a54f208b
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-01-28 10:01:00 +0000

    updated HeaderDivs to sanitize text

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -1,9 +1,11 @@
+import DOMPurify from "isomorphic-dompurify";
+
 export default function HeaderDivs({h1Content}: {h1Content: string}){
     return (
         <div className="w-full flex flex-col mx-auto grid grid-cols-6">
           <div className="border w-full flex flex-col border-black rounded-lg px-5 py-1 m-auto col-start-1 col-span-6">
             <div className="spacer"></div>
-            <h1>{h1Content}</h1>
+              <h1 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(h1Content)}}></h1>
             <div className="spacer"></div>
           </div>
         </div>
