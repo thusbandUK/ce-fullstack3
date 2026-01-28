5a98055b1d1647d7fd00bb4938decad0b2c26a40
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-01-28 10:00:29 +0000

    implemented Scroll component which auto scrolls to top of page when bugs would otherwise prevent that from happening

---
+++ app/ui/dashboard/Scroll.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+export default function Scroll() {
+	// when clicking a link, user will not scroll to the top of the page if the header is sticky.
+	// their current scroll position will persist to the next page.
+	// this useEffect is a workaround to 'fix' that behavior.
+
+    //from: https://github.com/vercel/next.js/issues/45187
+
+	const pathname = usePathname();
+	useEffect(() => {
+		window.scroll(0, 0);
+	}, [pathname]);
+	return <></>;
+}
