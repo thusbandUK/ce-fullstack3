4ace80f2f63518ce4dc061cf162244110bfe9f40
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-01-29 05:42:14 +0000

    created privacy route

---
+++ app/(overview)/privacy/page.tsx
@@ -0,0 +1,11 @@
+import HeaderDivs from "@/app/ui/dashboard/header"
+
+export default function Privacy(){
+
+    return (
+        <div>
+          <HeaderDivs h1Content="Privacy"></HeaderDivs>
+
+        </div>
+    )
+}
