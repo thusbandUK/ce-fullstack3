8c3157dc782fa64f1f1a921b73d12f8bab301b62
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-03 10:43:43 +0000

    updated sectionBorderless component with section semantic tags

--- app/ui/dashboard/sectionBorderless.tsx
+++ app/ui/dashboard/sectionBorderless.tsx
@@ -2,10 +2,14 @@ import clsx from "clsx"
 export default function SectionBorderless({children}: {children: React.ReactNode}){
 
     return (
+        <section>
         <div  className="md:grid md:grid-cols-6 gap-0 w-full items-center justify-center rounded-lg mt-[40] p-5">
+          
             <div className={clsx("md:col-start-2 md:col-span-6")}>
               {children}
             </div>
+         
         </div>
+        </section>
     )
 }
