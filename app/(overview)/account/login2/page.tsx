69cb630ecea98f8efe7652c23f6ea7ead12279c5
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-05 15:28:14 +0000

    commented out searchParams logic in login2

--- app/(overview)/account/login2/page.tsx
+++ app/(overview)/account/login2/page.tsx
@@ -1,12 +1,13 @@
 import { InitiateSignIn } from '../../../ui/initiateSignIn';
 import HeaderDivs from '../../../ui/dashboard/header';
 
-export default async function LoginPage({ searchParams }: { searchParams: Promise<{ location: string }> }) {
-//export default async function LoginPage() {    
+//export default async function LoginPage({ searchParams }: { searchParams: Promise<{ location: string }> }) {
+//export default async function ReceiveEmail({ searchParams }: { searchParams: Promise<{ location: string }> }){
+export default async function LoginPage() {    
 
   console.log('account login page 1')
-  const { location } = await searchParams;
-  //const location = ""
+  //const { location } = await searchParams;
+  const location = ""
   console.log('account login page 2')
   return (
     <>
