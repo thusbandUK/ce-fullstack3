64c9d9d0aa9900901bd309318087fcf68b04a974
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-05 14:28:15 +0000

    uncommented initiatesign in call on login2 page

--- app/(overview)/account/login2/page.tsx
+++ app/(overview)/account/login2/page.tsx
@@ -1,4 +1,4 @@
-//import { InitiateSignIn } from '../../../ui/initiateSignIn';
+import { InitiateSignIn } from '../../../ui/initiateSignIn';
 import HeaderDivs from '../../../ui/dashboard/header';
 
 //export default async function LoginPage({ searchParams }: { searchParams: Promise<{ location: string }> }) {
@@ -11,12 +11,12 @@ export default async function LoginPage() {
     <>
     <p>Hello!</p>
     {/**
-      <HeaderDivs h1Content='Sign in or sign up' />
+      <HeaderDivs h1Content='Sign in or sign up' />*/}
       <InitiateSignIn
         location={location ? location : ''}
         error={false}
       />
-       */}
+       
     </>
   );
 }
