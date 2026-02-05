8910dade5e56437821443c46098a0f4fb9ad2c06
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-05 14:59:23 +0000

    redirect link params omitted

--- app/(overview)/account/page.tsx
+++ app/(overview)/account/page.tsx
@@ -20,7 +20,8 @@ export default async function Account({ searchParams }: { searchParams: Promise<
   console.log('account base page 3')
   if (!session){
      //redirect(`/account/login?location=/account`);
-     redirect(`/account/login2?location=/account`);
+     //redirect(`/account/login2?location=/account`);
+     redirect(`/account/login2`);
    }
 
    console.log('account base page 4')
