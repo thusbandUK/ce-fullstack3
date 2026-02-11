c0df0f3e711e7da20af27ba3ee6abcc3b43894d3
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-11 10:31:46 +0000

    changed headerDivs comp to use client to debug dompurify problem

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -1,14 +1,13 @@
-//"use client"
-//<h1 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(h1Content)}}></h1>
-//import DOMPurify from "isomorphic-dompurify";
+"use client"
+
+import DOMPurify from "isomorphic-dompurify";
 
 export default function HeaderDivs({h1Content}: {h1Content: string}){
     return (
         <div className="w-full flex flex-col mx-auto grid grid-cols-6">
           <div className="border w-full flex flex-col border-black rounded-lg px-5 py-1 m-auto col-start-1 col-span-6">
             <div className="spacer"></div>
-              
-              <h1>{h1Content}</h1>
+              <h1 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(h1Content)}}></h1>
             <div className="spacer"></div>
           </div>
         </div>
