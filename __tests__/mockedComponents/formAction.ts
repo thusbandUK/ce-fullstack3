044ea7d7f75bb4ad93e8352285244ec7af5cb4df
Tom Husband <106232364+thusbandUK@users.noreply.github.com>
2026-02-11 09:15:40 +0000

    removed tests from git

--- __tests__/mockedComponents/formAction.ts
+++
@@ -1,6 +0,0 @@
-export default function formAction(_prevState: any, formData: FormData) {
-    const message = formData.get('message');
-    // Do something with message
-    return { success: true, message: "hello!", errors: {username: ["no errors, woop!"] }};
-  }
-  
